c1c56f455a593c05274e02fc900e5c78dd00148b
DoodleJZ <zhoujunru@sjtu.edu.cn>
2019-05-28 23:41:21 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ python src_joint/main.py test \
 --consttest-ptb-path data/23.auto.clean \
 --deptest-ptb-path data/ptb_test_3.3.0.sd \
 --embedding-path data/glove.gz \
---model-path-base models/joint_single.pt
+--model-path-base models/joint_cwt_best_dev=93.85_devuas=95.87_devlas=94.47
